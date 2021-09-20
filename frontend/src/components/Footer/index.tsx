a2179f8fd576ece66f54d8eeffdee1af6cac30ef
Douglas Vilas Boas <douglas_vilasboas@hotmail.com>
2021-09-20 19:24:32 -0300

    atualizacao

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/dvbhunter" target="_blank" rel="noreferrer">Douglas Vilas Boas</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/douglas-vilas-boas-36447753/?locale=pt_BR" target="_blank" rel="noreferrer">Douglas Vilas Boas</a></p>
+                <p className="text-light"><small><strong>Portifólio Online</strong><br />
+                    Aplicativo Web Desenvolvido em Java c/ Spring Boot no backend, typeScript com ReactJS no frontend: <a href="https://github.com/dvbhunter" target="_blank" rel="noreferrer">Clique para acessar os arquivos</a></small></p>
             </div>
         </footer>
     );
